8a6489c9ffec91d2d00ae9e93c4848fd1a6b967d
Ali Syihab <ali.syihab@gmail.com>
2021-05-29 18:28:57 +0700

    validate headers request

--- cypress/integration/task_3.js
+++ cypress/integration/task_3.js
@@ -1,6 +1,14 @@
 context('task-3', () => {
   describe('Get List User', () => {
     beforeEach(() => cy.request('https://reqres.in/api/users?page=2').as('users'));
+
+    it('Validate the header', function () {
+      cy.get('@users')
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json; charset=utf-8')
+    });
+
     it('should return response status code 200', function () {
       cy.get('@users').its('status').should('be.equal', 200)
     });
@@ -16,6 +24,13 @@ context('task-3', () => {
           job: "leader"
         }
       }).as('postUser');
+    });
+
+    it('Validate the header post users', function () {
+      cy.get('@postUser')
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json; charset=utf-8')
     });
 
     it('should return response status code 201', function () {
